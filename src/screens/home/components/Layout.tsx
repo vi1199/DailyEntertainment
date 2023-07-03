7838e019a1d5b3abd269b7fdcc67972339d65cba
vineet <nickel.caracara@gmail.com>
2023-07-03 23:17:44 +0530

    search autofill local functionality

--- src/screens/home/components/Layout.tsx
+++ src/screens/home/components/Layout.tsx
@@ -1,10 +1,13 @@
+import {StyleSheet} from 'react-native';
 import {VView} from '../../../ui';
 import {Colors} from '../../../ui/color';
 
 export function Layout({children}: {children: JSX.Element | JSX.Element[]}) {
-  return (
-    <VView style={{backgroundColor: Colors.Secondary['50'], flex: 1}}>
-      {children}
-    </VView>
-  );
+  return <VView style={styles.container}>{children}</VView>;
 }
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.Secondary['50'],
+    flex: 1,
+  },
+});
